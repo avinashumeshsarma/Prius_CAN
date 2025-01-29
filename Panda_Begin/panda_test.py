88efe2cd66ce4772c0b3c55e324f8ac2dd92ae92
avinashumeshsarma <66961602+avinashumeshsarma@users.noreply.github.com>
2025-01-29 15:26:12 +0100

    [AUS_VM] Reorganization

--- Panda_Begin/panda_test.py
+++ Panda_Begin/panda_test.py
@@ -2,5 +2,5 @@ from panda import Panda
 panda = Panda()
 # panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)  # Allow all CAN messages
 # panda.set_power_save(False)
-panda.can_recv()
-# print(panda.health())
+# panda.can_recv()
+print(panda.health())
